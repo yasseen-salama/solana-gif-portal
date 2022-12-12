66306cbf5a456f5fda4274da55edbbfdf49cfe10
Yasseen Salama <y.salama@tdsoftware.de>
2022-12-12 22:16:47 +0100

    Fixed LikersModal rendering more than once

--- src/App.js
+++ src/App.js
@@ -1,65 +1,64 @@
-import kp from './keypair.json'
-import LikersModal from './Components/LikersModal'
-import React, { useEffect, useState } from 'react';
-import TwitterHeart from 'twitter-heart';
-import twitterLogo from './assets/twitter-logo.svg';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import {Connection, PublicKey, clusterApiUrl} from '@solana/web3.js';
-import {Program, Provider, web3} from '@project-serum/anchor';
+import kp from "./keypair.json";
+import LikersModal from "./Components/LikersModal";
+import React, { useEffect, useState } from "react";
+import TwitterHeart from "twitter-heart";
+import twitterLogo from "./assets/twitter-logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Connection, PublicKey, clusterApiUrl } from "@solana/web3.js";
+import { Program, Provider, web3 } from "@project-serum/anchor";
 
 // SystemProgram is a reference to the Solana runtime!
 const { SystemProgram, Keypair } = web3;
 
-const arr = Object.values(kp._keypair.secretKey)
-const secret = new Uint8Array(arr)
+const arr = Object.values(kp._keypair.secretKey);
+const secret = new Uint8Array(arr);
 let baseAccount = web3.Keypair.fromSecretKey(secret);
 
-const programID = new PublicKey('CWtpJ9ETHrbqMC4HYXcd782BNoQsibHRqdeKsukB3wzd');
+const programID = new PublicKey("CWtpJ9ETHrbqMC4HYXcd782BNoQsibHRqdeKsukB3wzd");
 
 // Set our network to devnet.
-const network = clusterApiUrl('devnet');
-
-
+const network = clusterApiUrl("devnet");
 
 // Controls how we want to acknowledge when a transaction is "done".
 const opts = {
-  preflightCommitment: "processed"
-}
+  preflightCommitment: "processed",
+};
 
 // Constants
-const TWITTER_HANDLE = 'yasseen_salama';
+const TWITTER_HANDLE = "yasseen_salama";
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
-
   // State
   const [walletAddress, setWalletAddress] = useState(null);
-  const [inputValue, setInputValue] = useState('');
+  const [inputValue, setInputValue] = useState("");
   const [gifList, setGifList] = useState([]);
-  
-  // Likers Modal 
+
+  // Likers Modal
   const [modalState, setModalState] = useState(false);
   const [likers, setLikers] = useState([]);
-  const openModal = () => { setModalState(!modalState) };
-  const closeModal = () => { setModalState(!modalState) };
-
-//   React.useEffect(() => {setIsOpen(true);
-// }, [props.modalOpen])
-
+  const openModal = (likes) => {
+    setLikers(likes);
+    console.log(likes);
+    setModalState(!modalState);
+  };
+  const closeModal = () => {
+    setModalState(!modalState);
+  };
 
   const checkIfWalletIsConnected = async () => {
     try {
       const { solana } = window;
-      if(solana) {
-        if(solana.isPhantom){
+      if (solana) {
+        if (solana.isPhantom) {
           const response = await solana.connect({ onlyIfTrusted: true });
           setWalletAddress(response.publicKey.toString());
         }
       } else {
-        alert('Please get a Phantom wallet!');
+        alert("Please get a Phantom wallet!");
       }
-    } catch(error){
+    } catch (error) {
       console.log(error);
     }
   };
@@ -68,33 +67,33 @@ const App = () => {
     const { solana } = window;
     if (solana) {
       const response = await solana.connect();
-      console.log('Connected with Public Key:', response.publicKey.toString());
+      console.log("Connected with Public Key:", response.publicKey.toString());
       setWalletAddress(response.publicKey.toString());
     }
   };
-  
+
   const sendGif = async () => {
     if (inputValue.length === 0) {
-      console.log("No gif link given!")
-      return
+      console.log("No gif link given!");
+      return;
     }
-    setInputValue('');
-    console.log('Gif link:', inputValue);
+    setInputValue("");
+    console.log("Gif link:", inputValue);
     try {
-      const provider = getProvider()
-      const program = await getProgram(); 
-  
+      const provider = getProvider();
+      const program = await getProgram();
+
       await program.rpc.addGif(inputValue, {
         accounts: {
           baseAccount: baseAccount.publicKey,
           user: provider.wallet.publicKey,
         },
       });
-      console.log("GIF successfully sent to program", inputValue)
-  
+      console.log("GIF successfully sent to program", inputValue);
+
       await getGifList();
     } catch (error) {
-      console.log("Error sending GIF:", error)
+      console.log("Error sending GIF:", error);
     }
   };
 
@@ -105,56 +104,58 @@ const App = () => {
 
   const getProvider = () => {
     const connection = new Connection(network, opts.preflightCommitment);
-    const provider =  new Provider(connection, window.solana, opts.preflightCommitment,
-      );
+    const provider = new Provider(
+      connection,
+      window.solana,
+      opts.preflightCommitment
+    );
     return provider;
-  }
+  };
 
   const shortenAddress = (address) => {
-    return address.substring(0, 4) + '..' + address.substr(address.length - 4);
-  } 
-
-
+    return address.substring(0, 4) + ".." + address.substr(address.length - 4);
+  };
 
   const createGifAccount = async () => {
     try {
       const provider = getProvider();
       const program = await getProgram();
-      
-      console.log("ping")
+
+      console.log("ping");
       await program.rpc.startStuffOff({
         accounts: {
           baseAccount: baseAccount.publicKey,
           user: provider.wallet.publicKey,
           systemProgram: SystemProgram.programId,
         },
-        signers: [baseAccount]
+        signers: [baseAccount],
       });
-      console.log("Created a new BaseAccount w/ address:", baseAccount.publicKey.toString())
+      console.log(
+        "Created a new BaseAccount w/ address:",
+        baseAccount.publicKey.toString()
+      );
       await getGifList();
-  
-    } catch(error) {
-      console.log("Error creating BaseAccount account:", error)
+    } catch (error) {
+      console.log("Error creating BaseAccount account:", error);
     }
-  }
-  
+  };
 
   const renderConnectButton = () => (
     <button
       className="cta-button connect-wallet-button"
-      onClick={connectWallet}>
-        Connect Wallet
+      onClick={connectWallet}
+    >
+      Connect Wallet
     </button>
   );
 
   const heartClicked = (index) => {
     console.log(gifList[index].isLiked);
     if (gifList[index].isLiked) {
-      gifList[index].likes -=1;
+      gifList[index].likes -= 1;
       gifList[index].isLiked = false;
-    }
-    else {
-      gifList[index].likes +=1;
+    } else {
+      gifList[index].likes += 1;
       gifList[index].isLiked = true;
       likeGif(gifList[index].gifLink);
     }
@@ -164,97 +165,112 @@ const App = () => {
 
   const likeGif = async (gifLink) => {
     try {
-      const provider = getProvider()
-      const program = await getProgram(); 
-  
+      const provider = getProvider();
+      const program = await getProgram();
+
       await program.rpc.likeGif(gifLink, {
         accounts: {
           baseAccount: baseAccount.publicKey,
           user: provider.wallet.publicKey,
         },
       });
-      console.log("GIF successfully liked", gifLink);
-  
     } catch (error) {
-      console.log("Error liking GIF:", error)
+      console.log("Error liking GIF:", error);
     }
   };
-  
 
   const renderConnectedContainer = () => {
     // If true, program account hasn't been initialized.
-      if (gifList === null) {
-        console.log(gifList);
-        return (
-          <div className="connected-container">
-            <button className="cta-button submit-gif-button" onClick={createGifAccount}>
-              Do One-Time Initialization For GIF Program Account
-            </button>
-          </div>
-        )
-      }
-    // Otherwise, account exists. User can submit GIFs.
-  else {
-    return(
-      <div className="connected-container">
-        <form
-          onSubmit={(event) => {
-            event.preventDefault();
-            sendGif();
-          }}
-        >
-          <input
-            type="text"
-            placeholder="Enter GIF Link!"
-            value={inputValue}
-            onChange={onInputChange}
-          />
-          <button type="submit" className="cta-button submit-gif-button">
-            Submit
+    if (gifList === null) {
+      console.log(gifList);
+      return (
+        <div className="connected-container">
+          <button
+            className="cta-button submit-gif-button"
+            onClick={createGifAccount}
+          >
+            Do One-Time Initialization For GIF Program Account
           </button>
-        </form>
-        <div className="gif-grid">
-					{/* We use index as the key instead, also, the src is now item.gifLink */}
-          {gifList.map((item, index) => (
-            <div className="gif-item" key={index}>
-              <img src={item.gifLink}/>
-              <div className="gif-likes">
-                <div className="like-heart"><TwitterHeart isLiked={item.isLiked} onHeartClick={()=>heartClicked(index)}></TwitterHeart></div>
-                <div onClick={openModal} className="like-count"> {item.likes}</div>
-                <div className="gif-owner">
-                  <ul>
-                  <li> {shortenAddress(item.userAddress.toString())}</li>
-                  </ul>
+        </div>
+      );
+    }
+    // Otherwise, account exists. User can submit GIFs.
+    else {
+      return (
+        <div className="connected-container">
+          <form
+            onSubmit={(event) => {
+              event.preventDefault();
+              sendGif();
+            }}
+          >
+            <input
+              type="text"
+              placeholder="Enter GIF Link!"
+              value={inputValue}
+              onChange={onInputChange}
+            />
+            <button type="submit" className="cta-button submit-gif-button">
+              Submit
+            </button>
+          </form>
+          <div className="gif-grid">
+            {/* We use index as the key instead, also, the src is now item.gifLink */}
+            {gifList.map((item, index) => (
+              <div className="gif-item" key={index}>
+                <img src={item.gifLink} />
+                <div className="gif-likes">
+                  <div className="like-heart">
+                    <TwitterHeart
+                      isLiked={item.isLiked}
+                      onHeartClick={() => heartClicked(index)}
+                    ></TwitterHeart>
+                  </div>
+                  <div onClick={() => openModal(item.likers)} className="like-count">
+                    {" "}
+                    {item.likes}
+                  </div>
+                  <div className="gif-owner">
+                    <ul>
+                      <li> {shortenAddress(item.userAddress.toString())}</li>
+                    </ul>
+                  </div>
                 </div>
               </div>
-              <LikersModal modalState={modalState} onClose={closeModal} likersAddress={item.likers} />
-            </div>
-          ))}
+            ))}
+            <LikersModal
+              modalState={modalState}
+              onClose={closeModal}
+              likersAddress={likers}
+            />
+          </div>
         </div>
-        
-      </div>
-    )
-  }
-}
-const renderConnectedButton = () => {
-  // If true, program account hasn't been initialized.
-      return (
-          <button className="cta-button wallet-address-button">
-            <span className="icon">
-              <img src="https://3632261023-files.gitbook.io/~/files/v0/b/gitbook-legacy-files/o/spaces%2F-MVOiF6Zqit57q_hxJYp%2Favatar-1615495356537.png?generation=1615495356841399&alt=media" alt="Phantom_logo" width="20" height="20"></img>
-            </span>
-            {shortenAddress(walletAddress)}
-          </button>
-      )
+      );
     }
-  
+  };
+  const renderConnectedButton = () => {
+    // If true, program account hasn't been initialized.
+    return (
+      <button className="cta-button wallet-address-button">
+        <span className="icon">
+          <img
+            src="https://3632261023-files.gitbook.io/~/files/v0/b/gitbook-legacy-files/o/spaces%2F-MVOiF6Zqit57q_hxJYp%2Favatar-1615495356537.png?generation=1615495356841399&alt=media"
+            alt="Phantom_logo"
+            width="20"
+            height="20"
+          ></img>
+        </span>
+        {shortenAddress(walletAddress)}
+      </button>
+    );
+  };
 
   useEffect(() => {
     const onLoad = async () => {
       await checkIfWalletIsConnected();
-    };  
-    window.addEventListener('load', onLoad);
-    return () => window.removeEventListener('load', onLoad);
+    };
+    window.addEventListener("load", onLoad);
+    return () => window.removeEventListener("load", onLoad);
   }, []);
 
   const getProgram = async () => {
@@ -265,57 +281,57 @@ const renderConnectedButton = () => {
     // Create a program that you can call
     return new Program(idl, programID, getProvider());
   };
-  
-  const getGifList = async() => {
+
+  const getGifList = async () => {
     try {
-      const program = await getProgram(); 
-      const account = await program.account.baseAccount.fetch(baseAccount.publicKey);
-      
-      console.log("Got the account", account)
-      //TODO: change logic, this function gets called when a new GIF is added and threfore will set likes to 0  
+      const program = await getProgram();
+      const account = await program.account.baseAccount.fetch(
+        baseAccount.publicKey
+      );
+
+      console.log("Got the account", account);
+      //TODO: change logic, this function gets called when a new GIF is added and threfore will set likes to 0
       account.gifList.forEach(function (gif) {
-        gif.isLiked = false; 
+        gif.isLiked = false;
       });
-      setGifList(account.gifList)
+      setGifList(account.gifList);
     } catch (error) {
-      console.log("Error in getGifList: ", error)
+      console.log("Error in getGifList: ", error);
       setGifList(null);
     }
-  }
-  
+  };
+
   useEffect(() => {
     if (walletAddress) {
-      console.log('Fetching GIF list...');
-      getGifList()
+      console.log("Fetching GIF list...");
+      getGifList();
     }
   }, [walletAddress]);
 
   return (
-  <div className="app">
-    <div className={walletAddress ? 'authed-container' : 'container'}>
-      <div className='nav-bar'>
-        <div className="title">🖼️ GIF Portal on the Solana Blockchain</div>
-        {walletAddress && renderConnectedButton()} 
+    <div className="app">
+      <div className={walletAddress ? "authed-container" : "container"}>
+        <div className="nav-bar">
+          <div className="title">🖼️ GIF Portal on the Solana Blockchain</div>
+          {walletAddress && renderConnectedButton()}
         </div>
-        <p className="sub-text">
-          View your GIF collection in the metaverse ✨
-          </p>
-          {/* show the connect button only if we don't have a wallet address */}
-          {!walletAddress && renderConnectButton()}
+        <p className="sub-text">View your GIF collection in the metaverse ✨</p>
+        {/* show the connect button only if we don't have a wallet address */}
+        {!walletAddress && renderConnectButton()}
 
-      {/* show the GIFS if we have a wallet address */}
-      {walletAddress && renderConnectedContainer()}
+        {/* show the GIFS if we have a wallet address */}
+        {walletAddress && renderConnectedContainer()}
+      </div>
+      <div className="footer-container">
+        <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
+        <a
+          className="footer-text"
+          href={TWITTER_LINK}
+          target="_blank"
+          rel="noreferrer"
+        >{`built by @${TWITTER_HANDLE}`}</a>
+      </div>
     </div>
-    <div className="footer-container">
-      <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
-      <a
-        className="footer-text"
-        href={TWITTER_LINK}
-        target="_blank"
-        rel="noreferrer"
-      >{`built by @${TWITTER_HANDLE}`}</a>
-    </div>
-  </div>
   );
 };
 
--- src/Components/LikersModal.js
+++ src/Components/LikersModal.js
@@ -1,21 +1,38 @@
 import React from 'react';
 import '../Components/LikersModal.css'
 
-const LikersModal = ({ modalState, onClose, likersAddress }) => {
+const shortenAddress = (address) => {
+  return address.substring(0, 4) + '..' + address.substr(address.length - 4);
+} 
+
+const LikersModal = ({ modalState, onClose, likersAddress}) => {
   if (!modalState) {
     return null;
   }
-  console.log(likersAddress);
 
 
   return (
     <div className="modal-container">
       <div className="content">
-      <button onClick={onClose}>Close</button>
-        <h2>Likers</h2>
-        <ul>
-          {likersAddress.map((key, index) => <li key={index}>{key.toString()}</li>)}
-        </ul>
+        <div className="col-md-12 d-flex flex-row justify-content-between">
+          <h1 className="p-2">Likes</h1>
+          <button onClick={onClose} type="button" className="btn-close p-2"></button>
+          </div>
+       
+        
+          { likersAddress.map((key, index) => 
+          <div className="col-md-12 d-flex flex-row justify-content-between" key={index}>
+            <div className="p-2"> {shortenAddress(key.toString())}</div>
+            <div className="p-2">
+            <a href= {"https://explorer.solana.com/address/" + key.toString() + "?cluster=devnet"} >
+            <svg className="bi bi-box-arrow-up-right" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+  <path fillrule="evenodd" d="M8.636 3.5a.5.5 0 0 0-.5-.5H1.5A1.5 1.5 0 0 0 0 4.5v10A1.5 1.5 0 0 0 1.5 16h10a1.5 1.5 0 0 0 1.5-1.5V7.864a.5.5 0 0 0-1 0V14.5a.5.5 0 0 1-.5.5h-10a.5.5 0 0 1-.5-.5v-10a.5.5 0 0 1 .5-.5h6.636a.5.5 0 0 0 .5-.5z"/>
+  <path fillrule="evenodd" d="M16 .5a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0 0 1h3.793L6.146 9.146a.5.5 0 1 0 .708.708L15 1.707V5.5a.5.5 0 0 0 1 0v-5z"/>
+</svg>
+</a>
+</div>
+            </div>) }
+        
       </div>
     </div>
   );
